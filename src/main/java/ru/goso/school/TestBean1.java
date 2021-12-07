3c2e6aabb9bbb69e5fdcd53b2ba843029b5a56ff
Vladimir Golubev <goso@list.ru>
2021-12-08 00:41:03 +0300

    xml bean1

---
+++ src/main/java/ru/goso/school/TestBean1.java
@@ -0,0 +1,14 @@
+package ru.goso.school;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestBean1 {
+
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet murka = context.getBean("Мурка", Pet.class);
+        murka.say();
+        Pet tuzik = context.getBean("Тузик", Pet.class);
+        tuzik.say();
+    }
+}
