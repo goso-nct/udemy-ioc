6ce3a4d92b674dac953e78cd218d802cebf029ed
Vladimir Golubev <goso@list.ru>
2021-12-11 20:44:05 +0300

    @Value

--- src/main/java/ru/goso/school/Person.java
+++ src/main/java/ru/goso/school/Person.java
@@ -2,12 +2,14 @@ package ru.goso.school;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-
+    @Value("${person.name}")
     String name;
+    @Value("${person.age}")
     int age;
     @Autowired @Qualifier("dog")
     Pet pet;
--- src/main/java/ru/goso/school/TestAnnotation1.java
+++ src/main/java/ru/goso/school/TestAnnotation1.java
@@ -8,6 +8,8 @@ public class TestAnnotation1 {
         var context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         var person = context.getBean("person", Person.class);
+        System.out.println(person);
         person.callPet();
+
     }
 }
